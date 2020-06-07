710ab6bc7c487c77589baa9029d166328447276e
Marcin Michalczyk <marcinm312@outlook.com>
2020-06-07 21:07:38 +0200

    Refactoring

--- src/Dokument.java
+++ src/Dokument.java
@@ -1,25 +1,17 @@
 public class Dokument {
-	private String tytul;
-	private String tresc;
+    private final String tytul;
+    private final String tresc;
 
-	public Dokument(String tytul, String tresc) {
-		this.tytul = tytul;
-		this.tresc = tresc;
-	}
+    public Dokument(String tytul, String tresc) {
+        this.tytul = tytul;
+        this.tresc = tresc;
+    }
 
-	public void setTytul(String tytul) {
-		this.tytul = tytul;
-	}
+    public String getTytul() {
+        return tytul;
+    }
 
-	public String getTytul() {
-		return tytul;
-	}
-
-	public void setTresc(String tresc) {
-		this.tresc = tresc;
-	}
-
-	public String getTresc() {
-		return tresc;
-	}
+    public String getTresc() {
+        return tresc;
+    }
 }
